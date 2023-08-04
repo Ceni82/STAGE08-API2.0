0bb4d93f84200305432e60fba7e7dd18d219fd68
Mateus Ceni <98861755+Ceni82@users.noreply.github.com>
2023-08-03 21:02:45 -0300

    inner join

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,7 +8,11 @@ const notesRoutes = Router();
 
 const notesController= new NotesController();
 
+notesRoutes.get("/", notesController.index);
 notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/:id", notesController.show);
+notesRoutes.detele("/:id", notesController.delete);
+
 
 
 module.exports = notesRoutes;
